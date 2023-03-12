b62018e93e0e6933bccc67f91edda216ba8f6c5f
Prosperous-bowman <bowmanprosperousjr811@gmail.com>
2023-03-12 09:25:19 +0000

    Javascript function

---
+++ index.js
@@ -0,0 +1,34 @@
+function show_hide1(){
+  var click = document.getElementById("drop_content1");
+   if(click.style.display ==="none") {
+            click.style.display ="block";
+         } else {
+            click.style.display ="none";
+         }
+}
+
+function show_hide2(){
+  var click = document.getElementById("drop_content2");
+   if(click.style.display ==="none") {
+            click.style.display ="block";
+         } else {
+            click.style.display ="none";
+         }
+}
+
+function show_hide3(){
+  var click = document.getElementById("drop_content3");
+   if(click.style.display ==="none") {
+            click.style.display ="block";
+         } else {
+            click.style.display ="none";
+         }
+}
+function show_hide4(){
+  var click = document.getElementById("drop_content4");
+   if(click.style.display ==="none") {
+            click.style.display ="block";
+         } else {
+            click.style.display ="none";
+         }
+}
